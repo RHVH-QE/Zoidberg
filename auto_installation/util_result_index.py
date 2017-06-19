2f114ac3a88e1809833fcff60b889fec136fc09d
dracher <dracher@gmail.com>
2017-06-19 10:27:16 +0800

    add error handle when json is mal-formed

--- auto_installation/util_result_index.py
+++ auto_installation/util_result_index.py
@@ -10,10 +10,17 @@ def walk_the_logs():
     summary = dict()
     for dpath, dnames, files in os.walk(LOGS_DIR):
         if 'final_results.json' in files:
+            index = files.index('final_results.json')
             tmp = dpath.replace(LOGS_DIR, '').split('/')
             date = tmp[0]
             time_build = tmp[1] + '__' + tmp[2]
-            final_res = json.load(open(os.path.join(dpath, files[0])))['sum']
+            try:
+                final_res = json.load(
+                    open(os.path.join(dpath, files[index])))['sum']
+            except ValueError:
+                print("erros exists in file:: " + os.path.join(
+                    dpath, files[index]))
+                continue
 
             if summary.get(date, None):
                 summary[date][time_build] = [dnames, final_res]
@@ -24,4 +31,4 @@ def walk_the_logs():
 
 
 if __name__ == "__main__":
-    walk_the_logs()
+    pass
