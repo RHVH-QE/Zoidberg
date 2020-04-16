a00a26e6eb416d63237c43d05ebb6ddea5e7302a
Qin Yuan <qiyuan@redhat.com>
2020-04-16 16:27:33 +0800

    fix typo

--- auto_rhvh/checks/install/rhsm_check.py
+++ auto_rhvh/checks/install/rhsm_check.py
@@ -12,6 +12,6 @@ class RhsmCheck(object):
         ck01 = self.remotecmd.run_cmd('subscription-manager status | grep "Overall Status: Current"', timeout=300)
         ck02 = self.remotecmd.run_cmd('insights-client --status | grep "System is registered"', timeout=300)
         # need to unregister because entitlements are limited
-        self.remotecmd.run_cmd('subscription unregister', timeout=300)
+        self.remotecmd.run_cmd('subscription-manager unregister', timeout=300)
 
         return ck01[0] and ck02[0]
